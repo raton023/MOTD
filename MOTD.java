78269c239fc2806ce479234eb7b222399ac82717
Mario Rene Torres <volaras23@gmail.com>
2016-03-22 10:38:35 -0600

    Update MOTD.java

--- MOTD.java
+++ MOTD.java
@@ -11,7 +11,10 @@ import org.bukkit.craftbukkit.v1_9_R1.util.CraftIconCache;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.server.ServerListPingEvent;
+
 import com.mojang.authlib.GameProfile;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelDuplexHandler;
@@ -27,31 +30,46 @@ import net.minecraft.server.v1_9_R1.ServerPing.ServerPingPlayerSample;
 
 public class MOTD extends ChannelDuplexHandler implements Listener
 {
-public List<?> networkManagers;
+private List<?> networkManagers;
+static int conectados =0;
 
-public PacketStatusOutServerInfo motd(){
+public PacketStatusOutServerInfo darkania() {
 List<String> list = new ArrayList<>();
-list.add(ChatColor.GREEN+"Darkania");
-list.add(ChatColor.WHITE+"-------------------------");
-list.add(ChatColor.RED+"Estamos testiando plugins");
-list.add(ChatColor.RED+"Pronto estaremos online!");
+ServerPing ping = new ServerPing();
+if(Bukkit.getPluginManager().getPlugin("MOTD").getConfig().getBoolean("mant")){
+
+	//motd mantenimiento
+for(String elmsg:Bukkit.getPluginManager().getPlugin("MOTD").getConfig().getStringList("msglistmant")){list.add(ChatColor.translateAlternateColorCodes('&', elmsg));}
 GameProfile[] sample = new GameProfile[list.size()];
-for (int i = 0; i < list.size(); i++) {
-sample[i] = new GameProfile(UUID.randomUUID(), (String)list.get(i));
-}
+for (int i = 0; i < list.size(); i++) {sample[i] = new GameProfile(UUID.randomUUID(), (String)list.get(i));}
 ServerPingPlayerSample playerSample = new ServerPingPlayerSample(33, 2);//max,online players
 playerSample.a(sample);
-ServerPing ping = new ServerPing();
-ping.setMOTD(new ChatComponentText(ChatColor.BLUE+"Bienvenido a Darkania\n"+ChatColor.AQUA+"Online muy pronto..."));
+ping.setMOTD(new ChatComponentText(ChatColor.BLUE+"Bienvenido a Darkania\n"+ChatColor.translateAlternateColorCodes('&', Bukkit.getPluginManager().getPlugin("MOTD").getConfig().getString("mantmsg"))));
 ping.setPlayerSample(playerSample);
-ping.setServerInfo(new ServerData("Bienvenido Jugador", 107));//max,online players a String, version (-1 mantenimiento, 107 mc1.9)
+ping.setServerInfo(new ServerData("Mantenimiento", -1));
 ping.setFavicon(((CraftIconCache)Bukkit.getServerIcon()).value);
+}else{
+	
+	//motd normal
+for(String elmsg:Bukkit.getPluginManager().getPlugin("MOTD").getConfig().getStringList("msglist")){list.add(ChatColor.translateAlternateColorCodes('&', elmsg));}
+GameProfile[] sample = new GameProfile[list.size()];
+for (int i = 0; i < list.size(); i++) {sample[i] = new GameProfile(UUID.randomUUID(), (String)list.get(i));}
+ServerPingPlayerSample playerSample = new ServerPingPlayerSample(33, conectados);//max,online players
+playerSample.a(sample);
+ping.setMOTD(new ChatComponentText(ChatColor.BLUE+"Bienvenido a Darkania\n"+ChatColor.AQUA+"Bienvenido Jugador..."));
+ping.setPlayerSample(playerSample);
+ping.setServerInfo(new ServerData("Bienvenidos", 107));
+ping.setFavicon(((CraftIconCache)Bukkit.getServerIcon()).value);
+}
+
 return new PacketStatusOutServerInfo(ping);
 }
 
 
+
 public MOTD()
-{try{Field console = Bukkit.getServer().getClass().getDeclaredField("console");
+{try{
+Field console = Bukkit.getServer().getClass().getDeclaredField("console");
 console.setAccessible(true);
 Method[] arrayOfMethod;
 int j = (arrayOfMethod = ((MinecraftServer)console.get(Bukkit.getServer())).getServerConnection().getClass().getDeclaredMethods()).length;
@@ -60,9 +78,29 @@ Method method = arrayOfMethod[i];
 method.setAccessible(true);
 if (method.getReturnType() == List.class) {
 this.networkManagers = Collections.synchronizedList((List<?>)method.invoke(null, new Object[] { ((MinecraftServer)console.get(Bukkit.getServer())).getServerConnection() }));
-}}}catch (Exception e){e.printStackTrace();}}
+}}}
+catch (Exception e){e.printStackTrace();}
+}
+
 @EventHandler
-public void serverListPing(ServerListPingEvent event){
+public void viendomotd(ServerListPingEvent event) throws IllegalArgumentException, IllegalAccessException{
+Field field = null;
+Field[] arrayOfField;
+int j = (arrayOfField = NetworkManager.class.getDeclaredFields()).length;
+for (int i = 0; i < j; i++){
+Field field2 = arrayOfField[i];
+field2.setAccessible(true);
+if (field2.getType() == Channel.class) {field = field2;}}
+field.setAccessible(true);
+for (Object manager : networkManagers){
+Channel channel = (Channel)field.get(manager);
+if ((channel.pipeline().context("ping_handler") == null) && (channel.pipeline().context("packet_handler") != null)) {
+channel.pipeline().addBefore("packet_handler", "ping_handler", new MOTD());
+}}
+}
+@EventHandler
+public void entrando(PlayerJoinEvent event){
+conectados++;
 try{Field field = null;
 Field[] arrayOfField;
 int j = (arrayOfField = NetworkManager.class.getDeclaredFields()).length;
@@ -71,29 +109,26 @@ Field field2 = arrayOfField[i];
 field2.setAccessible(true);
 if (field2.getType() == Channel.class) {field = field2;}}
 field.setAccessible(true);
-for (Object manager : this.networkManagers){
+for (Object manager : networkManagers){
 Channel channel = (Channel)field.get(manager);
 if ((channel.pipeline().context("ping_handler") == null) && (channel.pipeline().context("packet_handler") != null)) {
-channel.pipeline().addBefore("packet_handler", "ping_handler", new MOTD());}}}catch (Exception e){e.printStackTrace();}
+channel.pipeline().addBefore("packet_handler", "ping_handler", new MOTD());
+}}}catch (Exception e){e.printStackTrace();}
+}
+
+@EventHandler
+public void saliendo(PlayerQuitEvent e){
+	conectados--;
 }
 @EventHandler
-public void onJoin(PlayerJoinEvent event)
-{try{Field field = null;
-Field[] arrayOfField;
-int j = (arrayOfField = NetworkManager.class.getDeclaredFields()).length;
-for (int i = 0; i < j; i++){
-Field field2 = arrayOfField[i];
-field2.setAccessible(true);
-if (field2.getType() == Channel.class) {field = field2;}}
-field.setAccessible(true);
-for (Object manager : this.networkManagers){
-Channel channel = (Channel)field.get(manager);
-if ((channel.pipeline().context("ping_handler") == null) && (channel.pipeline().context("packet_handler") != null)) {
-channel.pipeline().addBefore("packet_handler", "ping_handler", new MOTD());}}}catch (Exception e){e.printStackTrace();}
+public void saliendo(PlayerKickEvent e){
+	conectados--;
 }
+
+
 public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception{
 if ((msg instanceof PacketStatusOutServerInfo)){
-super.write(ctx, motd(), promise);return;}super.write(ctx, msg, promise);
+super.write(ctx, darkania(), promise);return;}super.write(ctx, msg, promise);
 }
 public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception
 {super.channelRead(ctx, msg);}
